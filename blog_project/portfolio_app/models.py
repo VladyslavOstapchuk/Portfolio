e3f2f5539f255f039deb066e729f0dc81b599ed2
Vladyslav Ostapchuk <vladyslav_ostapchuk@Vladyslavs-MacBook-Air.local>
2023-11-11 18:08:44 +0100

    Created database + work with admin page

--- blog_project/portfolio_app/models.py
+++ blog_project/portfolio_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Description of database table
+class Project(models.Model):
+    # title contains max 100 symbols
+    title = models.CharField(max_length=100)
+    # short description
+    description = models.CharField(max_length=250)
+    # upload_to contains folder where images will be uploaded
+    image = models.ImageField(upload_to='portfolio_app/images/')
+    # url to 
+    url = models.URLField(blank=True)
