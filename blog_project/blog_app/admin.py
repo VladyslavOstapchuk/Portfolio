e60793a9813cef23e4150d60300d4a932bc6ad56
Vladyslav Ostapchuk <vladyslav_ostapchuk@Vladyslavs-MacBook-Air.local>
2023-11-15 17:55:05 +0100

    Blog model was created and shown in admins page

--- blog_project/blog_app/admin.py
+++ blog_project/blog_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import models
+from .models import Blog
 
 # Register your models here.
+admin.site.register(Blog)
--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Blog(models.Model):
+    # title contains max 100 symbols
+    title = models.CharField(max_length=100)
+    # short description
+    description = models.CharField(max_length=250)
+    # upload_to contains folder where images will be uploaded
+    date = models.DateField(default=timezone.now)
