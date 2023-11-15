8dbffcd090d5cb5c02c94001c5333dd43e7605ff
Vladyslav Ostapchuk <vladyslav_ostapchuk@Vladyslavs-MacBook-Air.local>
2023-11-15 16:42:02 +0100

    Home page with list of database records was created.

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 # imported for static urls
 from django.conf.urls.static import static 
 from django.conf import settings
+from portfolio_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blog_project/portfolio_app/views.py
+++ blog_project/portfolio_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def home(req):
+    # Import of all data from DB
+    projects = Project.objects.all()
+
+    return render(
+    # Request
+    req,
+    # Path to html page template. Path should not contain template folder name
+    'portfolio/home.html', {
+    # Passing DB objects to the html template
+    'projects':projects})
