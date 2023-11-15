6e68648fa9f8477b2986430dc30338d361799ac4
Vladyslav Ostapchuk <vladyslav_ostapchuk@Vladyslavs-MacBook-Air.local>
2023-11-15 16:51:36 +0100

    Try to resolve detached head problem

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio_app import views
 
+# Links to pages
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
