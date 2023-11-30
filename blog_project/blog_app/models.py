c0c22e90373a273e57540982448259cd0c0fa6e2
Vladyslav Ostapchuk <vladyslav_ostapchuk@Vladyslavs-MacBook-Air.local>
2023-11-30 17:33:10 +0100

    More custom styles and static files were added. Website is filled with some content

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
     # title contains max 100 symbols
     title = models.CharField(max_length=100)
     # short description
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
     # upload_to contains folder where images will be uploaded
     date = models.DateField(default=timezone.now)
 
